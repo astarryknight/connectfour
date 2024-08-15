e51e980ea4ddc6ec7e90a9cb681d961131d6b7ea
Starry Knight <whiteh4tter@gmail.com>
2024-08-15 14:06:22 -0400

    fix while loop iteration error

--- main.js
+++ main.js
@@ -3,6 +3,31 @@ for (var i = 0; i < (7); i++) {
     column = document.createElement("div");
     column.id = "col" + i;
     column.classList.add("column");
+
+    column.addEventListener("click", (e) => {
+        e.target.id.length < 4 ? colnum = e.target.parentElement.id[3] : colnum = e.target.id[3] //makes sure that even if you click on a cell
+        //alert(e.target.id)
+        if (board[0][colnum] == null) {
+            var isnull = false
+            var ctr = 5;
+            while (!isnull) {
+                if (board[ctr][colnum] == null) {
+                    board[ctr][colnum] = "y";
+                    isnull = true;
+                }
+                ctr--;
+            }
+
+            console.log(board);
+
+            checkWin();
+            botMove();
+            checkWin();
+        } else {
+            alert("this column is already full!")
+        }
+    })
+
     document.getElementById("board").appendChild(column);
     for (var j = 0; j < 6; j++) {
         cell = document.createElement("div");
@@ -24,4 +49,12 @@ for (var i = 0; i < 6; i++) {
     for (var j = 0; j < 7; j++) {
         board[i].push(null);
     }
+}
+
+function botMove() {
+
+}
+
+function checkWin() {
+
 }
